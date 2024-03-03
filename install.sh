2d39f80856bc449b55f94c20aa363e8b1bfdb6b7
Vafeen <vaf_art@mail.ru>
2024-03-03 13:35:23 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo snap install code --classic
+sudo snap install android-studio --classic
+sudo snap install obsidian --classic
+sudo snap install telegram-desktop
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+sudo snap install intellij-idea-community --classic
+sudo apt install git 
+git config --global user.name "Vafeen"
+git config --global user.email "vaf_art@mail.ru"
+
